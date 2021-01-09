b88d29421b878433d92c9285d8dcb5e384e6bd8c
Taylor Bustamante <taylorbustamante16@gmail.com>
2021-01-09 13:22:02 -0800

    commit tree 22a78b5d1d1459ae34fe67fec144afff05ebc71a

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(arr, name) {
+  const new_arr = [];
+  for (let i = 0; i < arr.length; i++) {
+    new_arr.push(`Thank you, ${arr[i]}, for the wonderful ${name} gift!`);
+  }
+  return new_arr;
+}
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
